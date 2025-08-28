5f7ae4cf9e3ccbed829866c9a9e1adcf4ded52a8
Guilherme <ggomesns@gmail.com>
2025-08-28 15:22:37 -0300

    commit lista condicional
    Adicionando exercícios da lista de condicional

---
+++ Exercicio_I/Program.cs
@@ -0,0 +1,29 @@
+﻿// Implemente um programa , em C#, que solicite ao usuário digitar dois números
+//nteiros a e b, e de posse destes, imprimir qual o maior valor dos dois e, se for o caso,
+//apresente uma mensagem informando que os dois são iguais.
+
+using System;
+
+internal class Program
+{
+        private static void Main(string[] args)
+        {
+            int a, b;
+        Console.WriteLine("Digite o primeiro número inteiro (a):");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número inteiro (b):");
+        b = int.Parse(Console.ReadLine());
+        if (a > b)
+        {
+            Console.WriteLine($"O maior valor é: {a}");
+        }
+        else if (b > a)
+        {
+            Console.WriteLine($"O maior valor é: {b}");
+        }
+        else
+        {
+            Console.WriteLine("Os dois números são iguais.");
+        }
+    }
+}
---
+++ Exercicio_III/Program.cs
@@ -0,0 +1,24 @@
+﻿//Implemente um programa , em C#, que leia o nome de um dia da semana e escreva
+//    uma mensagem “FIM DE SEMANA” caso seja sábado ou domingo.
+
+
+using System;
+internal class Program
+{
+        private static void Main(string[] args)
+        {
+            string dia;
+        Console.WriteLine("Digite o nome de um dia da semana:");
+        dia = Console.ReadLine().ToLower();
+        while (dia != "segunda" && dia != "terça" && dia != "terca" && dia != "quarta" && dia != "quinta" && dia != "sexta" &&
+               dia != "sábado" && dia != "sabado" && dia != "domingo")
+        {
+            Console.WriteLine("Digite um dia da semana válido");
+            dia = Console.ReadLine().ToLower();
+        }
+        if (dia == "sábado" || dia == "sabado" || dia == "domingo")
+        {
+            Console.WriteLine("FIM DE SEMANA");
+        }
+    }
+}
